ffb61a9c085c26ddbaa7c396326f67d9f36b6f49
Paulina Oster <paulina.oster@gmail.com>
2019-04-03 16:07:04 +0200

    added buttons to Dashboard

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import Fab from '@material-ui/core/Fab';
+import { Link } from 'react-router-dom';
 
-const Dashboard = () => {
 
+class Dashboard extends React.Component {
+    render() {
+        return (
+            <div>
+                <Link to={"/search"}>
+                    <Fab
+                        color='primary'
+                        variant='extended'
+                    >
+                        Wyszukaj filmy
+                    </Fab>
+                </Link>
+                <Link to={"/watch-list"}><Fab
+                    color='primary'
+                    variant='extended'
+                >Sprawdź filmy do obejrzenia
+                </Fab></Link>
+                <Link to={"/add-rating"}><Fab
+                    color='primary'
+                    variant='extended'
+                >Dodaj ocenę/komentarz do filmu
+                </Fab></Link>
+                Dashboard
+        </div>
+        )
+    }
 }
 
 export default Dashboard
