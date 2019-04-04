205ff14901e3d5615fea249bfd4e9ad68aa33d6f
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-04-04 17:01:50 +0200

    Get searchTerm from text area and save it to state

--- src/components/FormSearch/FormSearch.js
+++ src/components/FormSearch/FormSearch.js
@@ -3,7 +3,7 @@ import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import MUISearch from '@material-ui/icons/Search';
 
-function FormSearch() {
+function FormSearch(props) {
     return (
         <div
             style={{width: '70%', margin: '15px auto', padding: '10px', boxShadow: '2px 2px 5px #333', borderRadius: '5px'}}
@@ -11,8 +11,8 @@ function FormSearch() {
             <TextField
                 fullWidth={true}
                 placeholder={'wpisz tytuł'}
-                //value={'props.searchTerm'}
-                //onChange={props.getSearchTerm}
+                value={props.searchTerm}
+                onChange={props.getSearchTerm}
                 margin="normal"
                 InputProps={{
                     startAdornment: (
--- src/views/Search/Search.js
+++ src/views/Search/Search.js
@@ -2,10 +2,22 @@ import React from 'react'
 import FormSearch from "../../components/FormSearch";
 
 class Search extends React.Component {
+    state = {
+        movies: null,
+        isLoading: false,
+        isError: false,
+        searchTerm: '',
+    };
+
+    getSearchTerm = (event) => this.setState({searchTerm: event.target.value});
+
     render(){
         return(
             <div>
-                <FormSearch/>
+                <FormSearch
+                    searchTerm={this.state.searchTerm}
+                    getSearchTerm={this.getSearchTerm}
+                />
 
             </div>
         )
