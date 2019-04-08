a4dae72cca25a6e4e5142c3f881db87ca7adb62d
PrzemekGdula <przemo.g12@wp.pl>
2019-04-08 15:11:05 +0200

    component SearchedList.js added

---
+++ src/components/SearchedList/SearchedList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const SearchedList = (props) => {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SearchedList
--- src/views/Search/Search.js
+++ src/views/Search/Search.js
@@ -1,10 +1,25 @@
 import React from 'react'
+// import FormSearch from "../../components/FormSearch";
+import SearchedList from "../../components/SearchedList";
 
 class Search extends React.Component {
-    render(){
-        return(
-            <div>
+    state = {
+        movies: null,
+        isLoading: false,
+        isError: false,
+        searchTerm: '',
+    };
 
+    getSearchTerm = (event) => this.setState({ searchTerm: event.target.value });
+
+    render() {
+        return (
+            <div>
+                {/* <FormSearch */}
+                searchTerm={this.state.searchTerm}
+                getSearchTerm={this.getSearchTerm}
+                <SearchedList />
+                />
 
             </div>
         )
