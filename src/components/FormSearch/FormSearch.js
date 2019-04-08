a5b9d2d67e039bd3408d97b8a31338412d01990e
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-04-08 15:49:54 +0200

    Get movies by year

--- src/components/FormSearch/FormSearch.js
+++ src/components/FormSearch/FormSearch.js
@@ -1,70 +1,83 @@
-import * as React from 'react';
+import React from 'react';
 
 import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import MUISearch from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
 import NativeSelect from '@material-ui/core/NativeSelect';
+import Slider from '@material-ui/lab/Slider';
 
-function FormSearch(props) {
-    const getMoviesBySearchTerm = (searchTerm, arrMovies) => {
-        fetch(`http://www.omdbapi.com/?apikey=526cfe10&s=${searchTerm}`)
-            .then(response => response.json())
-            .then(movies => arrMovies = movies.Search)
-            .then(arrMovies => console.log(arrMovies))
-    };
+class FormSearch extends React.Component {
 
-    const getMoviesByType = (type, searchTerm, arrMovies) => {
-        fetch(`http://www.omdbapi.com/?apikey=526cfe10&s=${searchTerm}&type=${type}`)
-            .then(response => response.json())
-            .then(movies => arrMovies = movies.Search)
-            .then(arrMovies => console.log(arrMovies))
-    };
 
-    return (
-        <div
-            style={{width: '70%', margin: '15px auto', padding: '10px', boxShadow: '2px 2px 5px #333', borderRadius: '5px'}}
-        >
-            <TextField
-                placeholder={'wpisz tytuł'}
-                value={props.searchTerm}
-                onChange={props.getSearchTerm}
-                margin="normal"
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <MUISearch/>
-                        </InputAdornment>
-                    ),
-                }}
-                style={{width: '90%', margin: '10px'}}
-            />
-
-            <Button
-                onClick={() => getMoviesBySearchTerm(props.searchTerm, props.movies)}
-                variant="contained"
-                color="primary"
-            >
-                wyszukaj
-            </Button>
-
-            <NativeSelect
+    render() {
+        return (
+            <div
                 style={{
-                    margin: '10px'}
-                }
-                value={props.type}
-                onChange={(event) => {
-                    props.getType(event);
+                    width: '70%',
+                    margin: '15px auto',
+                    padding: '10px',
+                    boxShadow: '2px 2px 5px #333',
+                    borderRadius: '5px'
                 }}
-                onClick={() => getMoviesByType(props.type, props.searchTerm, props.movies)}
             >
-                <option value="">Typ</option>
-                <option value={'movie'}>Film</option>
-                <option value={'series'}>Serial</option>
-                <option value={'episode'}>Odcinek</option>
-            </NativeSelect>
-        </div>
-    );
+                <TextField
+                    placeholder={'wpisz tytuł'}
+                    value={this.props.searchTerm}
+                    onChange={this.props.getSearchTerm}
+                    margin="normal"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <MUISearch/>
+                            </InputAdornment>
+                        ),
+                    }}
+                    style={{width: '60%', margin: '10px'}}
+                />
+
+                <Button
+
+                    onClick={() => this.props.getMoviesBySearchTerm(this.props.searchTerm)}
+                    variant="contained"
+                    color="primary"
+                >
+                    wyszukaj
+                </Button>
+
+                <NativeSelect
+                    style={{
+                        margin: '10px'
+                    }
+                    }
+                    value={this.props.type}
+                    onChange={(event) => {
+                        this.props.getType(event);
+                    }}
+                    onClick={() => this.props.getMoviesByType(this.props.type, this.props.searchTerm)}
+                >
+                    <option value="">Typ</option>
+                    <option value={'movie'}>Film</option>
+                    <option value={'series'}>Serial</option>
+                    <option value={'episode'}>Odcinek</option>
+                </NativeSelect>
+
+                <Slider
+                    style={{
+                        width: '90%',
+                        padding: '10px',
+                        margin: '0 auto'
+                    }}
+                    value={this.props.year}
+                    min={1950}
+                    max={2022}
+                    step={1}
+                    onChange={this.props.getYear}
+                    onDragEnd={() => this.props.getMoviesByYear(this.props.year, this.props.searchTerm)}
+                />
+            </div>
+        );
+    }
 }
 
 export default FormSearch
--- src/views/Search/Search.js
+++ src/views/Search/Search.js
@@ -8,12 +8,34 @@ class Search extends React.Component {
         isError: false,
         searchTerm: '',
         type: '',
-        year: null
+        year: 0
     };
 
     getSearchTerm = (event) => this.setState({searchTerm: event.target.value});
 
     getType = (event) => this.setState({type: event.target.value});
+
+    getYear = (event, year) => {
+        this.setState({ year });
+    };
+
+    getMoviesBySearchTerm = (searchTerm) => {
+        fetch(`http://www.omdbapi.com/?apikey=526cfe10&s=${searchTerm}`)
+            .then(response => response.json())
+            .then(arrMovies => this.setState({movies: arrMovies.Search}));
+    };
+
+    getMoviesByType = (type, searchTerm) => {
+        fetch(`http://www.omdbapi.com/?apikey=526cfe10&s=${searchTerm}&type=${type}`)
+            .then(response => response.json())
+            .then(arrMovies => this.setState({movies: arrMovies.Search}));
+    };
+
+    getMoviesByYear = (year, searchTerm) => {
+        fetch(`http://www.omdbapi.com/?apikey=526cfe10&s=${searchTerm}&y=${year}`)
+            .then(response => response.json())
+            .then(arrMovies => this.setState({movies: arrMovies.Search}));
+    };
 
     render(){
         return(
@@ -21,10 +43,14 @@ class Search extends React.Component {
                 <FormSearch
                     searchTerm={this.state.searchTerm}
                     getSearchTerm={this.getSearchTerm}
-                    movies={this.state.movies}
+                    getMoviesBySearchTerm={this.getMoviesBySearchTerm}
                     type={this.state.type}
                     getType={this.getType}
+                    getMoviesByType={this.getMoviesByType}
                     year={this.state.year}
+                    getYear={this.getYear}
+                    getMoviesByYear={this.getMoviesByYear}
+                    movies={this.state.movies}
                 />
 
             </div>
