2d59b6584826b38da8ef585093fbd1f3cc2bc8ec
PrzemekGdula <przemo.g12@wp.pl>
2019-04-09 14:33:53 +0200

    display poster and title

--- src/components/SearchedList/SearchedList.js
+++ src/components/SearchedList/SearchedList.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Avatar from '@material-ui/core/Avatar'
+
 
 
 const SearchedList = (props) => {
+
     return (
         <div>
-            {JSON.stringify(props.movies)}
+            {
+                props.movies.map(
+                    movie => (
+                        <ListItem>
+                            <ListItemAvatar>
+                                <img
+                                src={movie.Poster}
+                                />
+
+
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={movie.Title}
+                                secondary={movie.Year}
+                            />
+                        </ListItem>
+                    )
+                )
+            }
+
         </div>
     )
 }
