27f28b8368629de9309f5c4fd7c62d077b8017b6
Paulina Oster <paulina.oster@gmail.com>
2019-04-10 17:04:29 +0200

    created AddRating skeleton

--- src/views/AddRating/AddRating.js
+++ src/views/AddRating/AddRating.js
@@ -1,14 +1,48 @@
-import React from 'react'
+import React from 'react';
 
 class AddRating extends React.Component {
+    state = {
+        rating: null,
+        comment: "",
+        userName: "",
+        userEmail: "",
+    }
+
     render() {
         return (
             <div>
-                
-
+                <select
+                    onChange={this.handleRatingChange}
+                >
+                    {
+                        Array(5).fill(1).map((el, index) => (
+                            <option value={`${index}`}>{index}</option>
+                        ))
+                    }
+                </select>
+                <input
+                    type={''}
+                    value={this.state.comment}
+                    onChange={this.handleCommentInput}
+                />
+                <input
+                    type={''}
+                    value={this.state.comment}
+                    onChange={this.handleCommentInput}
+                />
+                <input
+                    type={''}
+                    value={this.state.comment}
+                    onChange={this.handleCommentInput}
+                />
+                <button
+                    onClick={this.onClickSubmitForm}
+                >
+                    Zapisz
+                </button>
             </div>
         )
     }
 }
 
-export default AddRating
+export default AddRating;
