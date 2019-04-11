d133371b0988042c13bd473e9b6cccdc300cdf01
PrzemekGdula <przemo.g12@wp.pl>
2019-04-11 16:42:42 +0200

    watch list added

--- src/views/WatchList/WatchList.js
+++ src/views/WatchList/WatchList.js
@@ -1,26 +1,40 @@
 import React from 'react'
+import SearchedList from "../../components/SearchedList";
 
 import { database } from '../../firebaseConfig'
 
 class WatchList extends React.Component {
     state = {
-        watchList: null
+        movies: null,
     }
 
     componentDidMount() {
         database.ref('watchlist').on(
             'value',
-            (snapshot) => this.setState({
-                watchList: snapshot.val()
-            })
+            (snapshot) => {
+                Promise.all(
+                    Object.keys(snapshot.val())
+                        .map(
+                            movieId => (
+                                fetch(`http://www.omdbapi.com/?apikey=526cfe10&i=${movieId}`)
+                                    .then(r => r.json())
+                            )
+                        )
+                )
+                    .then((movies) => this.setState({
+                        movies: movies
+                    }))
+            }
         )
     }
 
     render() {
         return (
             <div>
-
-
+                <SearchedList
+                    movies={this.state.movies}
+                    history={this.props.history}
+                />
             </div>
         )
     }
