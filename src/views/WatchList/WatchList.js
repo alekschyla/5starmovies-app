2ae4808ea0751619e20d0d0fdc0e146c35fb91e7
PrzemekGdula <przemo.g12@wp.pl>
2019-04-11 16:18:50 +0200

    WIP

--- src/views/WatchList/WatchList.js
+++ src/views/WatchList/WatchList.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
+import { database } from '../../firebaseConfig'
+
 class WatchList extends React.Component {
-    render(){
-        return(
+    state = {
+        watchList: null
+    }
+
+    componentDidMount() {
+        database.ref('watchlist').on(
+            'value',
+            (snapshot) => this.setState({
+                watchList: snapshot.val()
+            })
+        )
+    }
+
+    render() {
+        return (
             <div>
-                
+
 
             </div>
         )
