584eb0671e59f44c47c2c3bf63890c7b14a38c69
Paulina Oster <paulina.oster@gmail.com>
2019-04-26 16:23:37 +0200

    added logging user logIns in auth.js

--- src/state/auth.js
+++ src/state/auth.js
@@ -1,4 +1,4 @@
-import { auth, googleProvider } from '../firebaseConfig';
+import { auth, database, googleProvider } from '../firebaseConfig';
 import { setFavouritesMovieListActionCreator, setWatchlistMovieListActionCreator } from './movieList';
 import { setImdbIDActionCreator, setWatchlistActionCreator, setFavouritesActionCreator } from './movieDetails';
 import { clearMoviesDataActionCreator } from './movies';
@@ -11,6 +11,7 @@ const USER_NAME_CHANGED = 'auth/USER_NAME_CHANGED';
 const PASSW_CONF_CHANGED = 'auth/PASSW_CONF_CHANGED';
 const PASSW_CHECK = 'auth/PASSW_CHECK';
 const SET_USER = 'auth/SET_USER';
+const SET_USER_LOGIN_LOGS = 'auth/SET_USER_LOGIN_LOGS';
 
 const setUserActionCreator = user => ({
     type: SET_USER,
@@ -36,6 +37,10 @@ export const passwordCheckChangeActionCreator = (newPasswordCheck) => ({
     type: PASSW_CHECK,
     newPasswordCheck,
 });
+export const setUserLoginLogsActionCreator = (data) => ({
+    type: SET_USER_LOGIN_LOGS,
+    data,
+});
 
 export const startListeningToAuthChangeAsyncActionCreator = (
     () => (dispatch, getState) => {
@@ -47,13 +52,18 @@ export const startListeningToAuthChangeAsyncActionCreator = (
                     dispatch(changeUserNameActionCreator(''));
                     dispatch(passwordConfirmChangeActionCreator(''));
                     dispatch(changePasswordActionCreator(''));
+                    dispatch(saveUserDataActionCreator());
+                    dispatch(startListeningToUserLoginLogsAsyncCreator());
                 } else {
                     dispatch(setUserActionCreator(user));
+                    dispatch(stopListeningToUserLoginLogsAsyncCreator());
+                    dispatch(setUserLoginLogsActionCreator(null));
                 }
             }
         )
     }
 );
+
 export const registerUserActionCreator = () => (dispatch, getState) => {
     const state = getState();
     if (state.auth.passwordCheck) {
@@ -70,6 +80,7 @@ export const registerUserActionCreator = () => (dispatch, getState) => {
             .catch(error => console.log('wystąpił błąd', error));
     }
 };
+
 export const comparePasswordsActionCreator = (newPasswordConfirm) => (dispatch, getState) => {
     dispatch(passwordConfirmChangeActionCreator(newPasswordConfirm));
     const state = getState();
@@ -77,6 +88,7 @@ export const comparePasswordsActionCreator = (newPasswordConfirm) => (dispatch, 
         dispatch(passwordCheckChangeActionCreator(true))
         : dispatch(passwordCheckChangeActionCreator(false));
 };
+
 export const logInAsyncActionCreator = () => (dispatch, getState) => {
     const state = getState();
     const email = state.auth.email;
@@ -86,11 +98,13 @@ export const logInAsyncActionCreator = () => (dispatch, getState) => {
         .then(() => console.log('Zalogowano'))
         .catch(error => console.log('Wystąpił błąd', error))
 };
+
 export const logInByGoogleAsyncActionCreator = () => (dispatch, getState) => {
     auth.signInWithPopup(googleProvider)
         .then(() => console.log('Zalogowano'))
         .catch(error => console.log('Wystąpił błąd', error))
 };
+
 export const logOutAsyncActionCreator = () => (dispatch, getState) => {
     auth.signOut()
         .then(data => window.history.pushState(null, null, '/'))
@@ -106,6 +120,27 @@ export const logOutAsyncActionCreator = () => (dispatch, getState) => {
         });
 };
 
+export const saveUserDataActionCreator = () => (dispatch, getState) => {
+    const uid = getState().auth.user.uid;
+    const userData = {
+        timestamp: Date.now(),
+    };
+    database.ref(`database/users/${uid}/login`).push(userData);
+};
+
+export const startListeningToUserLoginLogsAsyncCreator = () => (dispatch, getState) => {
+    const uid = getState().auth.user.uid;
+    database.ref(`database/users/${uid}/login`).on(
+        'value',
+        snapshot => dispatch(setUserLoginLogsActionCreator(snapshot.val()))
+    );
+};
+
+export const stopListeningToUserLoginLogsAsyncCreator = () => (dispatch, getState) => {
+    const uid = getState().auth.user.uid;
+    database.ref(`database/users/${uid}/login`).off();
+};
+
 const initialState = {
     user: true,
     email: '',
@@ -113,6 +148,7 @@ const initialState = {
     userName: '',
     passwordConfirm: '',
     passwordCheck: true,
+    userLoginData: null,
 };
 
 export default (state = initialState, action) => {
@@ -127,33 +163,32 @@ export default (state = initialState, action) => {
                 ...state,
                 password: action.newValue,
             };
-
         case USER_NAME_CHANGED:
             return {
                 ...state,
                 userName: action.newValue,
             };
-
-
         case PASSW_CONF_CHANGED:
             return {
                 ...state,
                 passwordConfirm: action.newPasswordConfirm,
             };
-
         case PASSW_CHECK:
             return {
                 ...state,
                 passwordCheck: action.newPasswordCheck,
             };
-
         case SET_USER:
             return {
                 ...state,
                 user: action.user,
             };
-
+        case SET_USER_LOGIN_LOGS:
+            return {
+                ...state,
+                userLoginData: action.data,
+            };
         default:
-            return state
+            return state;
     }
 }
